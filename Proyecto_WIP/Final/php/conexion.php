7d8231550d097a66221aa62ec780a85434dabb62
Matt-ELR <131918424+Matt-ELR@users.noreply.github.com>
2024-05-30 08:27:47 -0600

    FixedNavBar adittion to Final folder

--- Proyecto_WIP/Final/php/conexion.php
+++ Proyecto_WIP/Final/php/conexion.php
@@ -1,4 +1,3 @@
 <?php
 #                   Ruta       User  pwd  BD
 $con = new mysqli('localhost','root','','colchoneria');
-/*Test1*/
--- Proyecto_WIP/Final/script.js
+++ Proyecto_WIP/Final/script.js
@@ -7,3 +7,16 @@ const backToTopButton = document.querySelector('.back-to-top');
 backToTopButton.addEventListener('click', () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+/*Test de cambios de JS*/
+
+const subHeader = document.querySelector('.sub-header');
+const scrollThreshold = 200; // adjust this value to your liking
+  
+window.addEventListener('scroll', () => {
+if (window.scrollY > scrollThreshold) {
+  subHeader.classList.add('fixed');
+} else {
+  subHeader.classList.remove('fixed');
+  }
+});
