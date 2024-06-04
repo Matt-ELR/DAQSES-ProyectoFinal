dada824acbfc66abea5b08bdcd9231d012c8bc9f
Matt-ELR <131918424+Matt-ELR@users.noreply.github.com>
2024-06-04 08:32:27 -0600

    Footer update

--- Proyecto_WIP/Final/php/conexion.php
+++ Proyecto_WIP/Final/php/conexion.php
@@ -1,3 +1,3 @@
 <?php
-#                   Ruta       User  pwd  BD
-$con = new mysqli('localhost','root','','colchoneria');
+#                   Ruta            User                pwd                 BD
+$con = new mysqli('localhost','id22231215_user','calificacion:4BMPG','id22231215_base1');
